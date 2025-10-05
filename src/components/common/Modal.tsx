10fe9045609ffbf951b0273381046ede7a160524
유림 <yulimh001@gmail.com>
2025-10-06 02:15:31 +0900

    feat(#1): 공통 모달 디자인 구현

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -3,7 +3,9 @@ import { ReactNode } from 'react'
 export default function Modal({ children, customClassName }: { children: ReactNode; customClassName?: string }) {
   return (
     <div className="fixed inset-0 z-60 flex items-center justify-center bg-[rgba(0,0,0,0.3)]">
-      <section className={`${customClassName} gap-y-s p-l flex w-[600px] flex-col rounded-[32px] bg-white`}>
+      <section
+        className={`${customClassName} desktop:w-[335px] mx-5 flex w-full flex-col gap-y-[31px] rounded-[20px] bg-white px-[14px] pt-[32px] pb-[13px]`}
+      >
         {children}
       </section>
     </div>
