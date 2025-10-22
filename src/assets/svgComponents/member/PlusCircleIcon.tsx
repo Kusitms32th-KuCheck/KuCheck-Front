2aa5546832b217692f9f058962c382eb7d618a98
유림 <yulimh001@gmail.com>
2025-10-23 06:31:17 +0900

    style(#19): 아이콘 추가

---
+++ src/assets/svgComponents/member/PlusCircleIcon.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgPlusCircleIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 32 32" {...props}><path stroke="#A9AABE" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.778} d="M16 29.336c7.364 0 13.334-5.97 13.334-13.334S23.364 2.67 16 2.67 2.667 8.639 2.667 16.002 8.637 29.336 16 29.336M16 10.669v10.667M10.667 16h10.667" /></svg>;
+export default SvgPlusCircleIcon;
