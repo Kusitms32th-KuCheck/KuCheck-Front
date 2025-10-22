2b67b6e2523a007a465106c2b742036e42dba8d4
유림 <yulimh001@gmail.com>
2025-10-22 23:06:58 +0900

    feat(#19): 내가 등록한 불참사유서 전체 조회

--- src/app/(member)/reason-for-absence/layout.tsx
+++ src/app/(member)/reason-for-absence/layout.tsx
@@ -1,3 +1,17 @@
+import MemberHeader from '@/components/member/common/MemberHeader'
+import { HelpCircleIcon } from '@/assets/svgComponents/member'
+
 export default function ReasonForAbsenceLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>
+  return (
+    <div>
+      <MemberHeader
+        headerType={'dynamic'}
+        title={'불참 사유서'}
+        rightElement={<HelpCircleIcon className="absolute right-5" width={20} height={20} />}
+      />
+      {/* 헤더의 높이만큼 공간 확보 */}
+      <div className="h-[117px]" />
+      {children}
+    </div>
+  )
 }
--- src/app/(member)/reason-for-absence/page.tsx
+++ src/app/(member)/reason-for-absence/page.tsx
@@ -1,3 +1,24 @@
-export default function ReasonForAbsencePage() {
-  return <></>
+import ReasonForAbsenceItem from '@/components/member/reason-for-absence/ReasonForAbsenceItem'
+import MemberButton from '@/components/member/common/MemberButton'
+import Link from 'next/link'
+
+export default async function ReasonForAbsencePage() {
+  return (
+    <main>
+      {/* 불참사유서 제출 기록 */}
+      <section className="px-5">
+        <ReasonForAbsenceItem />
+        <ReasonForAbsenceItem />
+        <ReasonForAbsenceItem />
+        <ReasonForAbsenceItem isLastIndex={true} />
+      </section>
+
+      {/* bottom button */}
+      <section className="fixed bottom-0 w-full bg-white px-5 pb-[24px]">
+        <MemberButton styleType={'primary'} buttonType={'button'} styleSize={'lg'} styleStatus={'default'}>
+          <Link href={'/reason-for-absence/submit'}>불참 사유서 제출하기</Link>
+        </MemberButton>
+      </section>
+    </main>
+  )
 }
