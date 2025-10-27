a96c6e095cdb2f691634790cd6f76e77a915bb38
ahcgnoej <jinjinchachajung@naver.com>
2025-10-28 02:51:00 +0900

    feat(#29): 운영진 관리 퍼블리싱

--- src/app/(manager)/staff-management/page.tsx
+++ src/app/(manager)/staff-management/page.tsx
@@ -1,3 +1,18 @@
+import TeamTable from '@/components/manager/staff-management/TeamTable'
+import { generateMockMembers } from '@/types/manager/member/mockData'
+
+const members = generateMockMembers()
+
 export default function StaffManagementPage() {
-  return <div>운영진 관리</div>
+  return (
+    <main className="flex h-full flex-col overflow-visible">
+      <p className="heading-lg-medium px-6 pt-8">운영진 관리</p>
+      <div className="px-6 pt-6">
+        <TeamTable teamName="학부학" members={members.slice(0, 3)} />
+        <TeamTable teamName="경영총괄팀" members={members.slice(0, 6)} />
+        <TeamTable teamName="교육기획팀" members={members.slice(0, 3)} />
+        <TeamTable teamName="대외홍보팀" members={[]} />
+      </div>
+    </main>
+  )
 }
---
+++ src/components/manager/staff-management/TeamTable.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import StaffTableRow from './staff-row/StaffTableRow'
+import type { Member } from '@/types/manager/member/mockData'
+import { PlusIcon } from '@/assets/svgComponents/manager'
+
+export default function TeamTable({ teamName, members }: { teamName: string; members: Member[] }) {
+  const gridTemplate = '133px 143px 181px 594px 172px 400px'
+
+  return (
+    <section className="mb-6 overflow-hidden rounded-[12px] bg-white">
+      <div className="flex items-center gap-3 px-6 pt-6 pb-5">
+        <p className="heading-lg-medium">{teamName}</p>
+        <button className="text-primary-500">
+          <PlusIcon width={24} height={24} />
+        </button>
+      </div>
+
+      <div>
+        {members && members.length > 0 ? (
+          <div className="scrollbar-hide overflow-x-auto">
+            <div
+              className="grid items-center border-b border-gray-100 p-[14px]"
+              style={{ gridTemplateColumns: gridTemplate }}
+            >
+              <p className="body-lg-medium pl-[24px] text-start text-gray-500">이름</p>
+              <p className="body-lg-medium px-[13px] text-start text-gray-500">파트</p>
+              <p className="body-lg-medium px-[13px] text-start text-gray-500">학교</p>
+              <p className="body-lg-medium px-[13px] text-start text-gray-500">학과</p>
+              <p className="body-lg-medium px-[13px] text-start text-gray-500">전화번호</p>
+              <p className="body-lg-medium px-[13px] text-start text-gray-500">로그인한 소셜 계정</p>
+            </div>
+
+            <div>
+              {members.map((m, i) => (
+                <StaffTableRow key={i} member={m} index={i} gridTemplate={gridTemplate} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="flex h-[120px] items-center justify-center text-gray-400">
+            <span className="body-lg-medium">+를 눌러 운영진을 등록해주세요</span>
+          </div>
+        )}
+      </div>
+    </section>
+  )
+}
