57076be63e2c8475384f2e3ff4ec652aea41f4cd
유림 <yulimh001@gmail.com>
2025-11-05 05:59:39 +0900

    style(#55): notice 디테일 페이지 디자인 수정

--- src/app/(member)/notice/[id]/page.tsx
+++ src/app/(member)/notice/[id]/page.tsx
@@ -1,4 +1,5 @@
 import MemberHeader from '@/components/member/common/MemberHeader'
+import { PaperclipIcon } from '@/assets/svgComponents/member'
 
 export default function NoticeDetailPage() {
   return (
@@ -11,6 +12,7 @@ export default function NoticeDetailPage() {
             <p className="heading-sm-semibold">🔊 아이디어 발표 & 커피챗 세션 ☕</p>
             <p className="body-sm-regular text-gray-400">9월 22일 19:00</p>
           </section>
+
           <section className="my-[23px] flex flex-col gap-y-[6px]">
             <div className="bg- bg-primary-50 flex h-[48px] items-center gap-x-[10px] rounded-[12px] pr-[10px] pl-4">
               <p className="body-sm-medium text-primary-500">장소</p>
@@ -21,33 +23,51 @@ export default function NoticeDetailPage() {
               <p className="body-sm-medium">9월 27일(토) 12:00 - 17:10</p>
             </div>
           </section>
-          <p>
-            안녕하세요. 학회원 여러분! 교육기획팀입니다. 이번 주 토요일에는 아이디어 발표 & 커피챗 세션이 진행됩니다.
-            🎤🌟
+
+          <p className="body-sm-regular text-gray-800">
+            안녕하세요. 학회원 여러분! 교육기획팀입니다.
+            <br /> 이번 주 토요일에는 아이디어 발표 & 커피챗 세션이 진행됩니다. 🎤🌟
             <br />
             <br />
             이번 세션은 추후 진행 될 밋업 프로젝트의 아이디어 후보군 9개를 공유하고, 학회원들이 원하는 아이디어에 지원할
             수 있도록 돕는 팀빌딩 프로세스의 첫 단계입니다! 각 발제자분들의 아이디어를 공유하고, 학회원들은 파트별로
             소그룹 대화를 나누며 깊이 있는 피드백을 나눌 수 있습니다.
           </p>
+
           <section className="my-[15px]">
-            <p className="body-sm-medium text-primary-500 py-[5px]">📁 팀 빌딩 결과.pdf</p>
-            <p className="body-sm-medium text-primary-500 py-[5px]">🔗 지원서 가이드라인 링크</p>
-            <p className="body-sm-medium text-primary-500 py-[5px]">🔗 발제된 9개 아이디어 확인</p>
+            <p className="body-sm-semibold text-primary-500 py-[5px]">지원서 가이드라인 링크</p>
           </section>
 
-          <p>
-            ☕ 커피챗 운영 방식
+          <div>
+            <p className="body-lg-semibold">☕ 커피챗 운영 방식</p>
             <br />
-            <br />
-            커피챗은 파트별(기획, 디자인, 프론트엔드, 백엔드)로 구성되며, 각 파트별 8명 기준, 2개 조로 나누어
-            운영됩니다. 한 조당 15분씩 발제자들이 학회원 테이블을 이동하며 대화를 나누는 방식입니다. 궁금하신 사항이
-            있으시면 자유롭게 발제자께 질문해주세요!
-          </p>
-          <div className="mt-[23px] flex flex-col gap-y-2">
+            <p className="body-sm-regular text-gray-800">
+              커피챗은 파트별(기획, 디자인, 프론트엔드, 백엔드)로 구성되며, 각 파트별 8명 기준, 2개 조로 나누어
+              운영됩니다. <br />
+              <br />한 조당 15분씩 발제자들이 학회원 테이블을 이동하며 대화를 나누는 방식입니다. 궁금하신 사항이
+              있으시면 자유롭게 발제자께 질문해주세요!
+            </p>
+          </div>
+
+          <section className="flex flex-col gap-y-2 py-[23px]">
+            <div className="flex items-center gap-x-2 rounded-[4px] bg-gray-100 px-[10px] py-3">
+              <PaperclipIcon width={20} height={20} />
+              <p className="body-sm-regular">
+                공지사항 첨부파일.pdf <span className="text-gray-500">(10mb)</span>
+              </p>
+            </div>
+            <div className="flex items-center gap-x-2 rounded-[4px] bg-gray-100 px-[10px] py-3">
+              <PaperclipIcon width={20} height={20} />
+              <p className="body-sm-regular">
+                공지사항 첨부파일.pdf <span className="text-gray-500">(10mb)</span>
+              </p>
+            </div>
+          </section>
+
+          <section className="flex flex-col gap-y-2">
             <div className="h-[335px] w-full rounded-[16px] bg-gray-100"></div>
             <div className="h-[188px] w-full rounded-[16px] bg-gray-100"></div>
-          </div>
+          </section>
         </div>
       </div>
     </main>
---
+++ src/assets/svgComponents/member/PaperclipIcon.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgPaperclipIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 20 20" {...props}><path stroke="#7E83A1" strokeLinecap="round" strokeLinejoin="round" d="M17.627 9.086 10.114 16.6a4.375 4.375 0 0 1-6.187-6.187L11.44 2.9a2.917 2.917 0 0 1 4.125 4.125l-7.219 7.218a1.458 1.458 0 0 1-2.062-2.062l6.335-6.335" /></svg>;
+export default SvgPaperclipIcon;
