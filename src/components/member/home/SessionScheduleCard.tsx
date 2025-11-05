34e63014a8a998ee1869cb8a928a4c67e038cb7c
유림 <yulimh001@gmail.com>
2025-11-05 13:42:24 +0900

    feat(#55): 데이터 불러오기 -> 스켈레톤 UI 구현

--- src/components/member/home/SessionScheduleCard.tsx
+++ src/components/member/home/SessionScheduleCard.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 
 import { BarIcon, ChevronRightIcon, ScheduleIcon } from '@/assets/svgComponents'
@@ -5,11 +6,16 @@ import { BarIcon, ChevronRightIcon, ScheduleIcon } from '@/assets/svgComponents'
 import { ThisWeekSessionDataType } from '@/types/member/session'
 import { formatToMonthDay } from '@/utils/common'
 
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import { useRouter } from 'next/navigation'
+
 interface SessionScheduleCardProps {
   sessionData: ThisWeekSessionDataType | undefined
 }
 
 export default function SessionScheduleCard({ sessionData }: SessionScheduleCardProps) {
+  const router = useRouter()
   /**
    * 시간 문자열에서 초(seconds)를 제거
    * @param timeString - 시간 문자열 (예: "15:02:00")
@@ -22,13 +28,22 @@ export default function SessionScheduleCard({ sessionData }: SessionScheduleCard
   }
 
   return (
-    <div className="flex w-full flex-col justify-between gap-y-[29px] rounded-[16px] bg-white px-[14px] pt-[17px] pb-[10px] shadow-[0_2px_12.9px_0_rgba(0,0,0,0.05)]">
+    <div
+      onClick={() => {
+        router.push('/session')
+      }}
+      className="flex w-full flex-col justify-between gap-y-[29px] rounded-[16px] bg-white px-[14px] pt-[17px] pb-[10px] shadow-[0_2px_12.9px_0_rgba(0,0,0,0.05)]"
+    >
       <div className="flex items-start justify-between">
-        <div className="flex items-start">
+        <div className="flex items-start gap-x-[5px]">
           <div className="flex h-[24px] w-[24px] items-center justify-center">
             <ScheduleIcon width={15} height={15} />
           </div>
-          <p className="body-lg-semibold pl-[5px]">{sessionData?.title}</p>
+          {sessionData?.title ? (
+            <p className="body-lg-semibold pl-[5px]">{sessionData?.title}</p>
+          ) : (
+            <Skeleton width={92} height={30} />
+          )}
         </div>
 
         <Link href={'/session'}>
@@ -36,15 +51,26 @@ export default function SessionScheduleCard({ sessionData }: SessionScheduleCard
         </Link>
       </div>
       <div className="flex flex-col gap-y-[3px] pl-[6px]">
-        <p className="caption-sm-medium text-gray-700">{sessionData?.place}</p>
+        {sessionData?.place ? (
+          <p className="caption-sm-medium text-gray-700">{sessionData?.place}</p>
+        ) : (
+          <Skeleton width={107} height={16}></Skeleton>
+        )}
+
         <div className="flex items-center gap-x-[6px]">
-          {sessionData?.startDate && (
+          {sessionData?.startDate ? (
             <p className="caption-sm-medium text-gray-700">{formatToMonthDay(sessionData?.startDate)}</p>
+          ) : (
+            <Skeleton width={26} height={16} />
           )}
           <BarIcon width={2} height={10} />
-          <p className="caption-sm-medium text-gray-700">
-            {removeSeconds(sessionData?.startTime)}~{removeSeconds(sessionData?.endTime)}
-          </p>
+          {sessionData?.startTime && sessionData?.endTime ? (
+            <p className="caption-sm-medium text-gray-700">
+              {removeSeconds(sessionData?.startTime)}~{removeSeconds(sessionData?.endTime)}
+            </p>
+          ) : (
+            <Skeleton width={69} height={16} />
+          )}
         </div>
       </div>
     </div>
