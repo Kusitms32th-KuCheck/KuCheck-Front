ea6e223563609eff8367f2db54ea1fd32c94791a
유림 <yulimh001@gmail.com>
2025-11-05 12:53:40 +0900

    style(#55): 알림 페이지 아이콘, 간격, 폰트 크기 조정

--- src/app/(member)/alarm/page.tsx
+++ src/app/(member)/alarm/page.tsx
@@ -6,12 +6,12 @@ export default function AlarmPage() {
     <main className="flex items-center justify-center bg-gray-100">
       <div className="desktop:w-[375px] min-h-screen w-full bg-white">
         <MemberHeader headerType="dynamic" title={'알림'} />
-        <div className="h-[116px] border" />
-        <div className="flex flex-col px-5">
+        <div className="h-[116px]" />
+        <div className="flex flex-col gap-y-1 px-5">
           <section className="flex items-center gap-x-[21px] py-[14px]">
-            <SessionNoticeBlueIcon width={28} height={28} />
+            <SessionNoticeBlueIcon width={32} height={32} />
             <div className="flex flex-col gap-y-[2px]">
-              <p className="body-sm-medium">4주차 세션 공지를 확인해보세요!</p>
+              <p className="body-lg-medium">4주차 세션 공지를 확인해보세요!</p>
               <div className="caption-sm-medium flex gap-x-[6px] text-gray-400">
                 <p className="">10/01</p>
                 <p>12:12</p>
@@ -19,9 +19,9 @@ export default function AlarmPage() {
             </div>
           </section>
           <section className="flex items-center gap-x-[21px] py-[14px]">
-            <PenaltyPointIcon width={28} height={28} />
+            <PenaltyPointIcon width={24} height={24} />
             <div className="flex flex-col gap-y-[2px]">
-              <p className="body-sm-medium">지각으로 벌점 -1점 기록되었어요</p>
+              <p className="body-lg-medium">지각으로 벌점 -1점 기록되었어요</p>
               <div className="caption-sm-medium flex gap-x-[6px] text-gray-400">
                 <p className="">10/01</p>
                 <p>12:12</p>
@@ -29,9 +29,9 @@ export default function AlarmPage() {
             </div>
           </section>
           <section className="flex items-center gap-x-[21px] py-[14px]">
-            <RewardPointIcon width={28} height={28} />
+            <RewardPointIcon width={24} height={24} />
             <div className="flex flex-col gap-y-[2px]">
-              <p className="body-sm-medium">큐포터즈 상점 등록이 완료되었어요</p>
+              <p className="body-lg-medium">큐포터즈 상점 등록이 완료되었어요</p>
               <div className="caption-sm-medium flex gap-x-[6px] text-gray-400">
                 <p className="">10/01</p>
                 <p>12:12</p>
