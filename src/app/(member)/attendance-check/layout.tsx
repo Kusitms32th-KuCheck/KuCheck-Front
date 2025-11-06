aa1f3ae42d82988a70148818c9010b4b39658f4e
유림 <yulimh001@gmail.com>
2025-11-07 07:02:29 +0900

    style(#61): 레이아웃 조정

--- src/app/(member)/attendance-check/layout.tsx
+++ src/app/(member)/attendance-check/layout.tsx
@@ -1,13 +1,7 @@
-import MemberHeader from '@/components/member/common/MemberHeader'
-
 export default function AttendanceCheckLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex items-center justify-center bg-gray-100">
-      <div className="desktop:w-[375px] bg-background1 relative min-h-screen w-full">
-        <MemberHeader headerType="dynamic" title={'출석체크'} headerColor={'bg-background1'} />
-        <div className="h-[116px]" />
-        {children}
-      </div>
+      <div className="desktop:w-[375px] bg-background1 relative min-h-screen w-full">{children}</div>
     </main>
   )
 }
