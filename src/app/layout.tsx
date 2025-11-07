521be699026c37ab8d9acf4adfcd69f6082eac93
유림 <yulimh001@gmail.com>
2025-11-07 17:32:34 +0900

    feat: 메이즈 연결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import QueryClientComponent from '@/components/member/common/QueryClient'
 import ToastContainer from '@/components/member/common/toast/ToastContainer'
 import { ToastProvider } from '@/components/member/common/toast/ToastContext'
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,10 +33,41 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const mazeApiKey = process.env.NEXT_PUBLIC_MAZE_API_KEY
+
   return (
     <html lang="en" className={pretendard.className}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <Script
+          id="maze-snippet"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function (m, a, z, e) {
+              var s, t, u, v;
+              try {
+                t = m.sessionStorage.getItem('maze-us');
+              } catch (err) {}
+              if (!t) {
+                t = new Date().getTime();
+                try {
+                  m.sessionStorage.setItem('maze-us', t);
+                } catch (err) {}
+              }
+              u = document.currentScript || (function () {
+                var w = document.getElementsByTagName('script');
+                return w[w.length - 1];
+              })();
+              v = u && u.nonce;
+              s = a.createElement('script');
+              s.src = z + '?apiKey=' + e;
+              s.async = true;
+              if (v) s.setAttribute('nonce', v);
+              a.getElementsByTagName('head')[0].appendChild(s);
+              m.mazeUniversalSnippetApiKey = e;
+            })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '${mazeApiKey}');`,
+          }}
+        />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${pretendard.variable} antialiased`}>
         <QueryClientComponent>
