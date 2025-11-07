9beb4f7268f88b4fe4c0a456bc52f8848575ae97
유림 <yulimh001@gmail.com>
2025-11-08 07:06:55 +0900

    style: 공지 데이터 변경

--- src/app/(member)/notice/[id]/page.tsx
+++ src/app/(member)/notice/[id]/page.tsx
@@ -1,61 +1,148 @@
 import MemberHeader from '@/components/member/common/MemberHeader'
 import { PaperclipIcon } from '@/assets/svgComponents/member'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function NoticeDetailPage() {
   return (
     <main className="flex items-center justify-center bg-gray-100">
       <div className="desktop:w-[375px] min-h-screen bg-white">
-        <MemberHeader headerType="dynamic" title={'세션 공지'} isBottomBorder={true} headerColor={'bg-white'} />
+        <MemberHeader headerType="dynamic" title={'공지'} isBottomBorder={true} headerColor={'bg-white'} />
         <div className="h-[116px]" />
         <div className="mt-[12px] px-5 pb-[145px]">
           <section className="flex flex-col gap-y-[3px] pt-[10px] pb-[8px]">
-            <p className="heading-sm-semibold">🔊 아이디어 발표 & 커피챗 세션 ☕</p>
-            <p className="body-sm-regular text-gray-400">9월 22일 19:00</p>
+            <p className="heading-sm-semibold">이달의 큐픽</p>
+            <p className="body-sm-regular text-gray-400">11월 07일 19:00</p>
           </section>
 
-          <p className="body-sm-regular mt-[31px] text-gray-800">
-            안녕하세요. 학회원 여러분! 교육기획팀입니다.
-            <br /> 이번 주 토요일에는 아이디어 발표 & 커피챗 세션이 진행됩니다. 🎤🌟
+          <div className="body-sm-regular mt-[20px] text-gray-800">
+            안녕하세요! 경영총괄팀 사무대관 담당 윤창현입니다
+            <br /> 이번 32기의 두 번째 큐픽, 11월의 큐픽이 찾아왔습니다 🎤🌟
             <br />
             <br />
-            이번 세션은 추후 진행 될 밋업 프로젝트의 아이디어 후보군 9개를 공유하고, 학회원들이 원하는 아이디어에 지원할
-            수 있도록 돕는 팀빌딩 프로세스의 첫 단계입니다! 각 발제자분들의 아이디어를 공유하고, 학회원들은 파트별로
-            소그룹 대화를 나누며 깊이 있는 피드백을 나눌 수 있습니다.
-          </p>
-
-          <section className="my-[15px]">
-            <p className="body-sm-semibold text-primary-500 py-[5px]">지원서 가이드라인 링크</p>
-          </section>
-
-          <div>
-            <p className="body-lg-semibold">☕ 커피챗 운영 방식</p>
+            <p className="body-md-semibold">큐픽이란?</p>
+            <ul className="mt-1 pl-5">
+              <li className="list-disc">
+                한 달에 한 번씩 열리는 온라인 세미나 강의로, 수강하시면 상점 1점이 부여됩니다!
+              </li>
+              <li className="list-disc">큐픽으로 한 달에 얻을 수 있는 최대 상점은 1점입니다.</li>
+            </ul>
             <br />
-            <p className="body-sm-regular text-gray-800">
-              커피챗은 파트별(기획, 디자인, 프론트엔드, 백엔드)로 구성되며, 각 파트별 8명 기준, 2개 조로 나누어
-              운영됩니다. <br />
-              <br />한 조당 15분씩 발제자들이 학회원 테이블을 이동하며 대화를 나누는 방식입니다. 궁금하신 사항이
-              있으시면 자유롭게 발제자께 질문해주세요!
-            </p>
+            <p className="body-md-semibold">이달의 큐픽 신청 방법</p>
+            <ul className="mt-1 pl-5">
+              <li className="list-disc">
+                각 큐픽에 해당하는 사이트 링크를 통해 개별적으로 지원해주세요. (단체를 쓰라고 하는 항목이 나온다면
+                ‘한국대학생IT경영학회‘를 쓰시면 됩니다.)
+              </li>
+              <li className="list-disc">개별 지원 후 신청 페이지를 캡처하여 @윤창현에게 보내주세요.</li>
+              <li className="list-disc">당일에 시청하고 있는 화면을 캡쳐 혹은 사진으로 인증해주시면 됩니다.</li>
+            </ul>
+            <br />
+            큐밀리 여러분의 많은 참여 부탁드립니다
           </div>
 
-          <section className="flex flex-col gap-y-2 py-[23px]">
-            <div className="flex items-center gap-x-2 rounded-[4px] bg-gray-100 px-[10px] py-3">
-              <PaperclipIcon width={20} height={20} />
-              <p className="body-sm-regular">
-                공지사항 첨부파일.pdf <span className="text-gray-500">(10mb)</span>
-              </p>
+          <div className="my-5 flex flex-col gap-y-4">
+            <div>
+              <p className="body-sm-semibold">1. [퍼즐 인사이트]2025년 마케팅 성공 사례 노하우 최초 공개,</p>
+              <Link
+                href={
+                  'https://event-us.kr/puzlcorporation/event/114578?utm_source=eventus&utm_medium=organic&utm_campaign=search-result'
+                }
+                className="text-primary-500 body-sm-regular"
+              >
+                [링크 바로가기]
+              </Link>
+              <ul className="body-sm-regular px-5">
+                <li className="list-disc">일시 : 11월 13일(목)</li>
+                <li className="list-disc">시간 : 16:00 ~ 18:00</li>
+                <li className="list-disc">장소 : YouTube</li>
+              </ul>
             </div>
-            <div className="flex items-center gap-x-2 rounded-[4px] bg-gray-100 px-[10px] py-3">
-              <PaperclipIcon width={20} height={20} />
-              <p className="body-sm-regular">
-                공지사항 첨부파일.pdf <span className="text-gray-500">(10mb)</span>
-              </p>
+
+            <div>
+              <p className="body-sm-semibold">2. [KISA] WHOIS OpenAPI를 활용한 도메인/IP 정보 알아보기</p>
+              <Link
+                href={
+                  'https://event-us.kr/Ut2KSHw11cCG/event/115543?utm_source=eventus&utm_medium=organic&utm_campaign=search-result'
+                }
+                className="text-primary-500 body-sm-regular"
+              >
+                [링크 바로가기]
+              </Link>
+              <ul className="body-sm-regular px-5">
+                <li className="list-disc">일시 : 11월 21일(금)</li>
+                <li className="list-disc">시간 : 10:00 ~ 11:30</li>
+                <li className="list-disc">장소 : 이벤터스 웨비나</li>
+              </ul>
             </div>
-          </section>
+
+            <div>
+              <p className="body-sm-semibold">
+                3. AI 모델 성능 관리, 이제는 데이터 경영의 시대 - 실제 사례로 보는 AI 성능 개선 전략
+              </p>
+              <Link
+                href={
+                  'https://event-us.kr/pebblous/event/115141?utm_source=eventus&utm_medium=organic&utm_campaign=search-result'
+                }
+                className="text-primary-500 body-sm-regular"
+              >
+                [링크 바로가기]
+              </Link>
+              <ul className="body-sm-regular px-5">
+                <li className="list-disc">일시 : 11월 18일(화)</li>
+                <li className="list-disc">시간 : 15:00 ~ 16:00</li>
+                <li className="list-disc">장소 : Zoom</li>
+              </ul>
+            </div>
+
+            <div>
+              <p className="body-sm-semibold">
+                4. 어디에서도 말하지 않은 K-브랜드의 내실 : 포트원의 재무 자동화 솔루션 웨비나 개최
+              </p>
+              <Link
+                href={
+                  'https://event-us.kr/preimatnc/event/115347?utm_source=eventus&utm_medium=organic&utm_campaign=search-result'
+                }
+                className="text-primary-500 body-sm-regular"
+              >
+                [링크 바로가기]
+              </Link>
+              <ul className="body-sm-regular px-5">
+                <li className="list-disc">일시 : 11월 28일(금)</li>
+                <li className="list-disc">시간 : 15:00 ~ 16:00</li>
+                <li className="list-disc">장소 : 기타</li>
+              </ul>
+            </div>
+          </div>
+
+          {/*<section className="flex flex-col gap-y-2 py-[23px]">*/}
+          {/*  <div className="flex items-center gap-x-2 rounded-[4px] bg-gray-100 px-[10px] py-3">*/}
+          {/*    <PaperclipIcon width={20} height={20} />*/}
+          {/*    <p className="body-sm-regular">*/}
+          {/*      공지사항 첨부파일.pdf <span className="text-gray-500">(10mb)</span>*/}
+          {/*    </p>*/}
+          {/*  </div>*/}
+          {/*  <div className="flex items-center gap-x-2 rounded-[4px] bg-gray-100 px-[10px] py-3">*/}
+          {/*    <PaperclipIcon width={20} height={20} />*/}
+          {/*    <p className="body-sm-regular">*/}
+          {/*      공지사항 첨부파일.pdf <span className="text-gray-500">(10mb)</span>*/}
+          {/*    </p>*/}
+          {/*  </div>*/}
+          {/*</section>*/}
 
           <section className="flex flex-col gap-y-2">
-            <div className="h-[335px] w-full rounded-[16px] bg-gray-100"></div>
-            <div className="h-[188px] w-full rounded-[16px] bg-gray-100"></div>
+            <div className="relative h-[200px] w-full rounded-[16px] bg-gray-100">
+              <Image src={'/ut/UT_01.png'} alt="이미지" fill className="rounded-[16px] object-cover" />
+            </div>
+            <div className="relative h-[200px] w-full rounded-[16px] bg-gray-100">
+              <Image src={'/ut/UT_02.png'} alt="이미지" fill className="rounded-[16px] object-cover" />
+            </div>
+            <div className="relative h-[200px] w-full rounded-[16px] bg-gray-100">
+              <Image src={'/ut/UT_03.jpg'} alt="이미지" fill className="rounded-[16px] object-cover" />
+            </div>
+            <div className="relative h-[200px] w-full rounded-[16px] bg-gray-100">
+              <Image src={'/ut/UT_04.jpg'} alt="이미지" fill className="rounded-[16px] object-cover" />
+            </div>
           </section>
         </div>
       </div>
--- src/components/member/notice/NoticeCard.tsx
+++ src/components/member/notice/NoticeCard.tsx
@@ -3,51 +3,59 @@
 import { useRouter } from 'next/navigation'
 
 import MemberTag from '@/components/member/common/MemberTag'
+import Image from 'next/image'
 
 interface NoticeCardProps {
   title: string
   tag: string
+  tagStyle?: string
   content: string
   date: string
-  imageUrlList?: string[]
+  imageUrl?: string
 }
 
-export default function NoticeCard({ title, tag, content, date, imageUrlList }: NoticeCardProps) {
+export default function NoticeCard({
+  title,
+  tag,
+  content,
+  date,
+  imageUrl,
+  tagStyle = 'bg-[#EFEAFF] text-[#6B42E0]',
+}: NoticeCardProps) {
   const router = useRouter()
   return (
     <div
       onClick={() => {
         router.push('/notice/1')
       }}
-      className="flex items-end gap-x-[15px] border-b border-gray-200 py-[20px]"
+      className="flex cursor-pointer items-end gap-x-[15px] border-b border-gray-200 py-[20px]"
     >
       <section className="flex flex-col">
         <div className="flex flex-col gap-y-[18px]">
           <div className="flex flex-col gap-y-[10px]">
-            <MemberTag status={'default'} type={'category'} customClassName="bg-[#EFEAFF] text-[#6B42E0] ">
+            <MemberTag status={'default'} type={'category'} customClassName={tagStyle}>
               {tag}
             </MemberTag>
             <div className="flex flex-col gap-y-[6px]">
               <p className="body-lg-medium">{title}</p>
-              <p className="body-sm-regular truncate">{content}</p>
+              <p className="body-sm-regular line-clamp-2">{content}</p>
             </div>
           </div>
 
           <p className="caption-sm-medium text-gray-400">{date}</p>
         </div>
       </section>
-      {imageUrlList
-        ? imageUrlList.map(() => (
-            <div className="relative flex h-[95px] w-[95px] shrink-0 rounded-[6px] border border-gray-200 whitespace-nowrap">
-              <>
-                <div className="absolute right-[7px] bottom-1 z-10 flex h-[14px] w-[14px] items-center justify-center rounded-[3px] bg-[#3A4047] text-white">
-                  <p className="font-pretendard text-[8px] font-medium text-white">2</p>
-                </div>
-                <div className="absolute right-1 bottom-[7px] h-[14px] w-[14px] rounded-[3px] bg-[#BDBDBD]" />
-              </>
+      {imageUrl ? (
+        <div className="relative flex h-[95px] w-[95px] shrink-0 rounded-[6px] border border-gray-200 whitespace-nowrap">
+          <Image fill src={imageUrl} alt="이미지" className="rounded-[6px] object-cover"></Image>
+          <>
+            <div className="absolute right-[7px] bottom-1 z-10 flex h-[14px] w-[14px] items-center justify-center rounded-[3px] bg-[#3A4047] text-white">
+              <p className="font-pretendard text-[8px] font-medium text-white">4</p>
             </div>
-          ))
-        : null}
+            <div className="absolute right-1 bottom-[7px] h-[14px] w-[14px] rounded-[3px] bg-[#BDBDBD]" />
+          </>
+        </div>
+      ) : null}
     </div>
   )
 }
