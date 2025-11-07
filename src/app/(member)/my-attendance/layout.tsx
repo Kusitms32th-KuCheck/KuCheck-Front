214f01068f39c1a3cc942ea5ebc87cbb816f7942
유림 <yulimh001@gmail.com>
2025-11-07 16:13:10 +0900

    hotfix: 상벌점 확인 헤더 라이팅 수정

--- src/app/(member)/my-attendance/layout.tsx
+++ src/app/(member)/my-attendance/layout.tsx
@@ -4,7 +4,7 @@ export default function MyAttendanceLayout({ children }: { children: React.React
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="desktop:w-[375px] bg-background1 relative flex min-h-screen w-full flex-col overflow-y-scroll">
-        <MemberHeader headerType="dynamic" title={'나의 출석'} headerColor={'bg-background1'} />
+        <MemberHeader headerType="dynamic" title={'상벌점 확인'} headerColor={'bg-background1'} />
         <div className="h-[116px]" />
         {children}
       </div>
