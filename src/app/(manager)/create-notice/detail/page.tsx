455e16fc7d00c16935cc71b45884bcde503b9876
ahcgnoej <jinjinchachajung@naver.com>
2025-11-13 22:11:14 +0900

    feat(#77): 공지글 상세 퍼블리싱

---
+++ src/app/(manager)/create-notice/detail/page.tsx
@@ -0,0 +1,9 @@
+import NoticeDetail from '../../../../components/manager/create-notice/notice-detail/NoticeDetail'
+
+export default function CreateNoticeDetailPage() {
+  return (
+    <main className="flex h-full flex-col overflow-visible">
+      <NoticeDetail />
+    </main>
+  )
+}
