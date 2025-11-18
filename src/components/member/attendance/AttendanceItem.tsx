93dad6140f981bf06d6c41f743143a84c9fcc8ff
유림 <yulimh001@gmail.com>
2025-11-18 10:18:59 +0900

    style(#88): 내 상벌점 조회 하단 버튼 제거, 상벌점 색상 변경

--- src/components/member/attendance/AttendanceItem.tsx
+++ src/components/member/attendance/AttendanceItem.tsx
@@ -5,33 +5,9 @@ interface AttendanceItemProps {
   record: AttendanceCheckRecordType
 }
 
-// 포인트 매핑 객체
-const POINTS_MAP: Record<AttendanceEnumType, number> = {
-  KUPICK: 1,
-  TF: 2,
-  STAFF: 1,
-  STUDY: 1,
-  KUPORTERS: 1,
-  PRESENT_HOLIDAY: 1,
-  PRESENT: 0,
-  EXCUSED: 0,
-  ABSENT: -3,
-  ABSENT_WITH_DOC: -2,
-  LATE: -1,
-  EARLY_LEAVE: -1,
-}
-
 export default function AttendanceItem({ record }: AttendanceItemProps) {
-  // 포인트 계산
-  const calculatePoints = (attendanceEnumType: AttendanceEnumType): string => {
-    const points = POINTS_MAP[attendanceEnumType]
-    if (points === undefined) return '0'
-    return points >= 0 ? `+${points}` : `${points}`
-  }
-
   // 포인트에 따른 색상 반환
-  const getPointsColor = (attendanceEnumType: AttendanceEnumType): string => {
-    const points = POINTS_MAP[attendanceEnumType]
+  const getPointsColor = (points: number): string => {
     if (points === undefined || points === 0) return 'text-gray-700'
     if (points > 0) return 'text-primary-500'
     return 'text-sub-red'
@@ -53,7 +29,7 @@ export default function AttendanceItem({ record }: AttendanceItemProps) {
           )}
         </div>
       </div>
-      <p className={`${getPointsColor(record.type)} body-sm-semibold text-gray-700`}>{calculatePoints(record.type)}</p>
+      <p className={`${getPointsColor(record.points)} body-sm-semibold text-gray-700`}>{record.points}</p>
     </section>
   )
 }
