431d49e17f76fde9480d85ffc2a9d159bda6fe0a
유림 <yulimh001@gmail.com>
2025-11-22 04:49:09 +0900

    style(#99): 마진 값 10px 추가

--- src/app/(member)/my-attendance/page.tsx
+++ src/app/(member)/my-attendance/page.tsx
@@ -106,7 +106,7 @@ export default function MyAttendancePage() {
           </div>
         </section>
 
-        <section className="mt-[44px] flex w-full flex-1 flex-col items-center justify-center gap-y-[24px]">
+        <section className="mt-[54px] flex w-full flex-1 flex-col items-center justify-center gap-y-[24px]">
           {allRecords.length === 0 ? (
             <div className="flex items-center justify-center">
               <p className="body-lg-medium text-gray-500">아직 받은 상벌점이 없어요</p>
