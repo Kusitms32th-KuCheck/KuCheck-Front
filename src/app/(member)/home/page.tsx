6e19f281779862b0fbc926056f291df1ce4f8956
유림 <yulimh001@gmail.com>
2026-01-07 18:05:08 +0900

    fix: home 서버 캐싱 없애기

--- src/app/(member)/home/page.tsx
+++ src/app/(member)/home/page.tsx
@@ -8,8 +8,6 @@ import Banner from '@/components/member/home/Banner'
 import { getSessionThisWeek } from '@/lib/member/server/session'
 import DirectLink from '@/components/member/home/DirectLink'
 
-export const revalidate = 60
-
 export default async function HomePage() {
   const [profileSummaryResult, sessionResult] = await Promise.all([getProfileSummary(), getSessionThisWeek()])
 
